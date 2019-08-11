5e126daab1f072fff6e4f43c0f1d1a67476ba9c1
Zuzana Jeschke <zuz@zuzjes.com>
2019-08-11 10:37:18 +0200

    add gauges and google analytics

--- pages/index.js
+++ pages/index.js
@@ -114,6 +114,30 @@ export default () => {
           }
         `}
       </style>
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-133283927-1', 'auto');
+        ga('send', 'pageview');
+      </script>
+      <script type="text/javascript">
+        var _gauges = _gauges || [];
+        (function() {
+          var t   = document.createElement('script');
+          t.type  = 'text/javascript';
+          t.async = true;
+          t.id    = 'gauges-tracker';
+          t.setAttribute('data-site-id', '5c4c99e1e2780409622de4ab');
+          t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+          t.src = 'https://d36ee2fcip1434.cloudfront.net/track.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(t, s);
+        })();
+      </script>
     </>
   );
 };
+
