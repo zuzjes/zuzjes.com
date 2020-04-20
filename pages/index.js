2a08579cd591c0b1b24c3877a30168081bb5adfc
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-20 21:12:27 +0200

    redesign page

--- pages/index.js
+++ pages/index.js
@@ -7,116 +7,91 @@ import instagram from '../assets/icons/instagram.svg'
 import resume from '../assets/icons/resume.svg'
 import Gauges from '../components/Gauges'
 import GoogleAnalytics from '../components/GoogleAnalytics'
+import styled, { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-size: 1.5em;
+  }
+`
+const Container = styled.div`
+  padding-left: 40px;
+  font-family: 'IBM Plex Sans', sans-serif;
+  max-width: 800px;
+`
+const Header = styled.h1`
+  font-size: 2.8em;
+  margin-block-end: 0.2em;
+`
+const Technologies = styled.b`
+  font-size: 1.7em;
+`
+const A = styled.a`
+  color: #088958;
+`
+const Icon = styled.a`
+  padding: 0.25em;
+  &:first-child {
+    padding-left: 0;
+  }
+`
 export default () => {
   return (
     <>
+      <GlobalStyle />
       <Head>
         <title>Zuzka Jeschke - zuzjes.com</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
-          href="https://fonts.googleapis.com/css?family=Cutive+Mono&display=swap"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <div className="w-45 container">
-        <h1>
-          <b>Zuzka Jeschke</b>
-        </h1>
+      <Container>
+        <Header>Zuzka Jeschke</Header>
         <p>
-          Jsem velký nadšenec do hor, lezení a do{' '}
-          <a href="https://www.instagram.com/jsemnela/" target="_blank">
-            Nely
-          </a>
-          .
+          Jsem velký nadšenec do hor, lezení,{' '}
+          <A href="https://www.instagram.com/jsemnela/">Nely</A>
+          (muj pes) a svých kytek.
         </p>
         <p>
-          Věnuji se převážně <b>frontendu</b>, jednou chci být hustá
-          programátorka. Proto se teď snažím do sebe nasát co nejvíce znalostí a
-          zkušeností. Baví mě dělat věci efektivně a smysluplně. I když jsem
-          trochu introvert, práce v týmu je moje věc!
+          Věnuji se <b>frontendu</b>, jednou chci být ale hustá programátorka.
+          Proto se teď snažím do sebe nasát co nejvíce znalostí a zkušeností.
+          Baví mě dělat věci efektivně a smysluplně. I když jsem trochu
+          introvert, po osmělení se umim bavit, práce v týmu je moje věc!
         </p>
-        <p>React, Next.js, JS, Now, Git, Bootstrap, Styled Components</p>
+        <Technologies>
+          React, Next.js, JS, Now, Git, Gitlab, Bootstrap, React Bootstrap,
+          Styled Components
+        </Technologies>
         <p>
-          <a href="tel:+420608540464">+420 608 540 464</a>(
-          <a href="https://t.me/zuzjes">Telegram</a>)<br />
-          <a href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</a>
+          <A href="tel:+420608540464">+420 608 540 464</A>
+          <br />
+          <A href="https://t.me/zuzjes">Telegram</A>
+          <br />
+          <A href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</A>
+          <br />
+          <A href="/resume.pdf">CV</A>
         </p>
         <p className="crow">
-          <a
-            href="https://twitter.com/zuzjes"
-            className="padd-5"
-            target="_blank"
-          >
+          <Icon href="https://twitter.com/zuzjes">
             <img src={twitter} alt="Twitter" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/zuzjes/"
-            className="padd-5"
-            target="_blank"
-          >
+          </Icon>
+          <Icon href="https://www.linkedin.com/in/zuzjes/">
             <img src={linkedin} alt="Linkedin" />
-          </a>
-          <a
-            href="https://github.com/zuzjes"
-            className="padd-5"
-            target="_blank"
-          >
+          </Icon>
+          <Icon href="https://github.com/zuzjes">
             <img src={github} alt="Github" />
-          </a>
-          <a
-            href="https://www.instagram.com/zuzjes/"
-            className="padd-5"
-            target="_blank"
-          >
+          </Icon>
+          <Icon href="https://www.instagram.com/zuzjes/">
             <img src={instagram} alt="Instagram" />
-          </a>
-          <a
-            href="https://www.facebook.com/zuzka.jeschke"
-            className="padd-5"
-            target="_blank"
-          >
+          </Icon>
+          <Icon href="https://www.facebook.com/zuzka.jeschke">
             <img src={facebook} alt="facebook" />
-          </a>
-          <a href="/resume.pdf" className="padd-5" target="_blank">
-            <img src={resume} alt="CV-pdf" height="60" />
-          </a>
+          </Icon>
         </p>
-      </div>
-      <style jsx>
-        {`
-          .w-45 {
-            max-width: 45%;
-          }
-          .padd-5 {
-            padding: 5px;
-          }
-          .container {
-            padding-right: 15px;
-            padding-left: 15px;
-            margin-right: auto;
-            margin-left: auto;
-            font-family: 'Cutive Mono', monospace;
-            max-width: 800px;
-          }
-          h1 {
-            font-size: 3em;
-          }
-          p,
-          a {
-            font-size: 30px;
-          }
-          a {
-            color: #ff5296;
-          }
-          ::selection {
-            background: #b5ebde; /* WebKit/Blink Browsers */
-          }
-          ::-moz-selection {
-            background: #b5ebde; /* Gecko Browsers */
-          }
-        `}
-      </style>
+      </Container>
+
       <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
       <GoogleAnalytics google_analytics_site_id="UA-133283927-1" />
     </>
