c3cfd860f2a8be3d99349622477361b43f0c9927
Zuzana Jeschke <zuz@zuzjes.com>
2021-04-04 09:23:12 +0200

    Remove unused return tag

--- pages/index.js
+++ pages/index.js
@@ -44,58 +44,57 @@ Button.defaultProps = {
   target: "_blank"
 };
 
-export default () => {
-  return (
-    <>
-      <GlobalStyle />
-      <Head>
-        <title>Zuzka Jeschke - zuzjes.com</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Container>
-        <Header>Zuzka Jeschke</Header>
-        <p>
-          Jsem velký nadšenec do hor, lezení,{" "}
-          <A href="https://www.instagram.com/jsemnela/">Nely</A> (muj pes) a
+export default () => (
+  <>
+    <GlobalStyle />
+    <Head>
+      <title>Zuzka Jeschke - zuzjes.com</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <Container>
+      <Header>Zuzka Jeschke</Header>
+      <p>
+        Jsem velký nadšenec do hor, lezení,{" "}
+        <A href="https://www.instagram.com/jsemnela/">Nely</A> (muj pes) a
           svých <A href="https://www.instagram.com/zuz_kytky/">kytek</A>.
         </p>
-        <p>
-          Věnuji se <b>frontendu</b>, jednou chci být hustá programátorka. Proto
+      <p>
+        Věnuji se <b>frontendu</b>, jednou chci být hustá programátorka. Proto
           se teď snažím do sebe nasát co nejvíce znalostí a zkušeností. Baví mě
           dělat věci efektivně a smysluplně. I když jsem trochu introvert, po
           osmělení se umím bavit, práce v týmu je moje věc!
         </p>
-        <Technologies>
-          React, Next.js, JS, Now, Git, Gitlab, Bootstrap, Material-UI, Formik,
-          Styled&nbsp;Components
+      <Technologies>
+        React, Next.js, JS, Now, Git, Gitlab, Bootstrap, Material-UI, Formik,
+        Styled&nbsp;Components
         </Technologies>
-        <p>
-          <A href="tel:+420608540464">+420 608 540 464</A> (
+      <p>
+        <A href="tel:+420608540464">+420 608 540 464</A> (
           <A href="https://t.me/zuzjes">Telegram</A>)
           <br />
-          <A href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</A>
+        <A href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</A>
+        <br />
+        <A href="https://zuzj.es/">zuzj.es</A> (rozcestník)
           <br />
-          <A href="https://zuzj.es/">zuzj.es</A> (rozcestník)
-          <br />
-          <A href="/resume.pdf">CV</A>
-        </p>
-        <p className="row">
-          <Button href="https://github.com/zuzjes">Github</Button>
-          <Button href="https://www.linkedin.com/in/zuzjes/">Linkedin</Button>
-          <Button href="https://twitter.com/zuzjes">Twitter</Button>
-          <Button href="https://www.instagram.com/zuzjes/">Instagram</Button>
-          <Button href="https://www.facebook.com/zuzka.jeschke">
-            Facebook
+        <A href="/resume.pdf">CV</A>
+      </p>
+      <p className="row">
+        <Button href="https://github.com/zuzjes">Github</Button>
+        <Button href="https://www.linkedin.com/in/zuzjes/">Linkedin</Button>
+        <Button href="https://twitter.com/zuzjes">Twitter</Button>
+        <Button href="https://www.instagram.com/zuzjes/">Instagram</Button>
+        <Button href="https://www.facebook.com/zuzka.jeschke">
+          Facebook
           </Button>
-        </p>
-      </Container>
+      </p>
+    </Container>
 
-      <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
-      <GoogleAnalytics google_analytics_site_id="UA-133283927-1" />
-    </>
-  );
-};
+    <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
+    <GoogleAnalytics google_analytics_site_id="UA-133283927-1" />
+  </>
+);
+
