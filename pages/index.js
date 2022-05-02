dde08762eedb8725773b618412ac389fa08c1c4f
Zuzana Jeschke <zuz@zuzjes.com>
2022-05-02 22:48:06 +0200

    Edit styling of page

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,24 @@ import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-size: 1.5em;
+    font-size: 1.3em;
+    background: white;
+    min-height: 100vh;
+    padding: 0 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  ::selection {
+  background: #25e8da; 
+  }
+  p {
+    line-height: 30px;
   }
 `;
+
 const Container = styled.div`
   padding: 0 2em;
   font-family: "IBM Plex Sans", sans-serif;
@@ -16,30 +31,47 @@ const Container = styled.div`
     padding: 0 1em;
   }
 `;
+
 const Header = styled.h1`
-  font-size: 2.8em;
-  margin-block-end: 0.2em;
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  color:white;
 `;
-const Technologies = styled.b`
-  font-size: 1.7em;
+
+const Background = styled.span`
+  background-color: #19a89e;
+  border-radius: 12px;
+  padding: 15px 8px;
 `;
+
+const Content = styled.div`
+  margin: 40px 0 20px 0;
+`;
+
 const A = styled.a`
-  color: #088958;
+  color: #19a89e;
+  font-weight: bold;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
+
 const Button = styled.a`
-  border: 2px solid #088958;
-  padding: 0.15em 0.4em;
-  margin: 0.3em 0.6em 0.3em 0;
-  color: #088958;
+  border: 2px solid #ade0dd;
+  border-radius: 12px;
+  padding: .15em .4em .23em .4em;
+  color: #53ada7;
   text-decoration: none;
   display: inline-block;
   &:hover {
-    border: 2px solid #088958;
-    padding: 0.15em 0.4em;
-    background-color: #088958;
+    border: 2px solid #ade0dd;
+    background-color: #ade0dd;
     color: white;
   }
 `;
+
 Button.defaultProps = {
   target: "_blank"
 };
@@ -55,42 +87,27 @@ export default () => (
         rel="stylesheet"
       />
     </Head>
+    
     <Container>
-      <Header>Zuzka Jeschke</Header>
-      <p>
-        Jsem velký nadšenec do hor, lezení,{" "}
-        <A href="https://www.instagram.com/jsemnela/">Nely</A> (muj pes) a
-          svých <A href="https://www.instagram.com/zuz_kytky/">kytek</A>.
-        </p>
-      <p>
-        Věnuji se <b>frontendu</b>, jednou chci být hustá programátorka. Proto
-          se teď snažím do sebe nasát co nejvíce znalostí a zkušeností. Baví mě
-          dělat věci efektivně a smysluplně. I když jsem trochu introvert, po
-          osmělení se umím bavit, práce v týmu je moje věc!
-        </p>
-      <Technologies>
-        React, Next.js, JS, Now, Git, Gitlab, Bootstrap, Material-UI, Formik,
-        Styled&nbsp;Components
-        </Technologies>
-      <p>
-        <A href="tel:+420608540464">+420 608 540 464</A> (
-          <A href="https://t.me/zuzjes">Telegram</A>)
-          <br />
+      <Header>
+        <Background> Zuzka Jeschke </Background>
+      </Header>
+      <Content>
+        <A href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</A><br/><br/>
+        <A href="https://t.me/zuzjes">Telegram</A><br/>
         <A href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</A>
-        <br />
-        <A href="https://zuzj.es/">zuzj.es</A> (rozcestník)
+        <br /><br />
+        <A href="https://zuzj.es/">zuzj.es</A>
           <br />
         <A href="/resume.pdf">CV</A>
-      </p>
-      <p className="row">
-        <Button href="https://github.com/zuzjes">Github</Button>
-        <Button href="https://www.linkedin.com/in/zuzjes/">Linkedin</Button>
-        <Button href="https://twitter.com/zuzjes">Twitter</Button>
-        <Button href="https://www.instagram.com/zuzjes/">Instagram</Button>
-        <Button href="https://www.facebook.com/zuzka.jeschke">
-          Facebook
-          </Button>
-      </p>
+      </Content>
+      <Content className="text-center">
+        <Button style={{marginRight: "10px"}} href="https://github.com/zuzjes">Github</Button>
+        <Button style={{marginRight: "10px"}} href="https://www.linkedin.com/in/zuzjes/">Linkedin</Button>
+        <Button style={{marginRight: "10px"}} href="https://twitter.com/zuzjes">Twitter</Button>
+        <Button style={{marginRight: "10px"}} href="https://www.instagram.com/zuzjes/">Instagram</Button>
+        <Button href="https://www.facebook.com/zuzka.jeschke">Facebook</Button>
+      </Content>
     </Container>
 
     <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
